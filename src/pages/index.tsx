16a88ea9dd025694040bada762b413d72601c1fa
Aditya Bhattad <adityabhattad18@gmail.com>
2023-04-30 16:10:31 +0530

    Router Settings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,8 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
-  console.log(user);
+  const {data}=api.posts.getAll.useQuery();
   
 
   return (
@@ -22,6 +21,13 @@ const Home: NextPage = () => {
         <div>
           {!user.isSignedIn && <SignInButton/>}
           {!!user.isSignedIn && <SignOutButton/>}
+        </div>
+        <div>
+          {data?.map((post)=>(
+            <div key={post.id}>
+                {post.content}
+            </div>
+          ))}
         </div>
       </main>
     </>
